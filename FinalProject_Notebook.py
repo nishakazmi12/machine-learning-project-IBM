948cc12ee36b5a1e339228227398b532ee1d3c2b
Nisha Kazmi <59197825+nishakazmi12@users.noreply.github.com>
2025-06-04 21:48:10 +0500

    Update FinalProject_Notebook.py

--- FinalProject_Notebook.py
+++ FinalProject_Notebook.py
@@ -19,13 +19,13 @@ def __iter__(self): return 0
 # @hidden_cell
 # The following code accesses a file in your IBM Cloud Object Storage. It includes your credentials.
 # You might want to remove those credentials before you share the notebook.
-client_d15662382c424fb392e350350f5abbc7 = ibm_boto3.client(service_name='s3',
-    ibm_api_key_id='D8wmsEInRmGhLw3cIwaUdZALHVZI-PRb_ELibHRT_D0E',
+client_d156623850350f = ibm_boto3.client(service_name='s3',
+    ibm_api_key_id='D8wmsEInRmGhLw3cIwaUdZALHV',
     ibm_auth_endpoint="https://iam.cloud.ibm.com/oidc/token",
     config=Config(signature_version='oauth'),
     endpoint_url='https://s3-api.us-geo.objectstorage.service.networklayer.com')
 
-body = client_d15662382c424fb392e350350f5abbc7.get_object(Bucket='finalprojectcoursera-donotdelete-pr-nabcaau098l0pp',Key='loan_train.csv')['Body']
+body = client_d15662382c424get_object(Bucket='finalprojectcoursera-donotdelete-pr-nabcaau098l0pp',Key='loan_train.csv')['Body']
 # add missing __iter__ method, so pandas accepts body as file-like object
 if not hasattr(body, "__iter__"): body.__iter__ = types.MethodType( __iter__, body )
 
